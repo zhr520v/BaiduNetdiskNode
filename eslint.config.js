1ce20fb5d3a6634c06c76d66da2372f5017c3367
keenghost <keenghost@163.com>
2024-08-10 16:51:33 +0800

    chore: use new eslint config version

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import tsESLint from 'typescript-eslint'
+
+export default tsESLint.config({
+  files: ['**/*.js', '**/*.cjs', '**/*.ts', '**/*.tsx'],
+  extends: [tsESLint.configs.base],
+  rules: {
+    eqeqeq: 'error',
+    'newline-before-return': 'error',
+    'no-debugger': 'error',
+
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+      },
+    ],
+
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'block',
+      },
+      {
+        blankLine: 'always',
+        prev: 'block',
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'block-like',
+      },
+      {
+        blankLine: 'always',
+        prev: 'block-like',
+        next: '*',
+      },
+    ],
+
+    'lines-between-class-members': [
+      'error',
+      {
+        enforce: [
+          //   {
+          //     blankLine: 'never',
+          //     prev: 'field',
+          //     next: 'field',
+          //   },
+          {
+            blankLine: 'always',
+            prev: '*',
+            next: 'method',
+          },
+          {
+            blankLine: 'always',
+            prev: 'method',
+            next: '*',
+          },
+        ],
+      },
+    ],
+  },
+})
