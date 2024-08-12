d615f082a6d759adac27028d00702cb12fa1754e
keenghost <keenghost@163.com>
2024-08-12 22:01:33 +0800

    chore: add test script

---
+++ scripts/test.ts
@@ -0,0 +1,25 @@
+import child_process from 'child_process'
+import fs from 'fs'
+import minimist from 'minimist'
+import pico from 'picocolors'
+
+const args = minimist(process.argv.slice(2))
+const pros = args._
+
+if (pros.length === 0) {
+  console.error(pico.red(`${pico.bgRed(pico.white(' ERROR '))} No projects specified.`))
+  process.exit(0)
+}
+
+for (const pro of pros) {
+  if (!fs.existsSync(`packages/${pro}`)) {
+    console.error(
+      pico.red(`${pico.bgRed(pico.white(' ERROR '))} Project ${pico.bold(pro)} does not exist.`)
+    )
+    process.exit(0)
+  }
+}
+
+for (const pro of pros) {
+  child_process.execSync('pnpm test', { cwd: `packages/${pro}`, stdio: 'inherit' })
+}
