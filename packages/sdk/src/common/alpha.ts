916d0949167f7485a20558d16e5bb1cec5b7c5c8
keenghost <keenghost@163.com>
2024-08-29 00:49:24 +0800

    chore: pkg-sdk add shared const ES PRESV_ENC_BLOCK_SIZE

--- packages/sdk/src/common/alpha.ts
+++ packages/sdk/src/common/alpha.ts
@@ -1,1 +1,4 @@
+export const __ES__ = !(typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+export const __PRESV_ENC_BLOCK_SIZE__ = 32
+
 export type PromType<T> = T extends Promise<infer A> ? A : never
