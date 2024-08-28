42148c37dfb32ef682ba9c985fac6ebd980c60d0
keenghost <keenghost@163.com>
2024-08-29 00:47:43 +0800

    feat: pkg-sdk utils add PromBat encrypt decrypt md5 tryTimes

--- packages/sdk/src/common/utils.ts
+++ packages/sdk/src/common/utils.ts
@@ -1,3 +1,5 @@
+import crypto from 'crypto'
+
 export function pick<T extends {}, K extends keyof T>(inObj: T, inKeys: K[]) {
   const obj = inObj as { [key: string]: any }
   const keys = inKeys as string[]
@@ -24,4 +26,74 @@ export function omit<T extends {}, K extends keyof T>(inObj: T, inKeys: K[]) {
   }
 
   return result as Omit<T, K>
+}
+
+export class PromBat<T = void> {
+  #prom: Promise<T>
+  #res: (inValue: T | PromiseLike<T>) => void = () => {}
+  #rej: (inReason?: any) => void = () => {}
+
+  constructor() {
+    this.#prom = new Promise<T>((res, rej) => {
+      this.#res = res
+      this.#rej = rej
+    })
+  }
+
+  get prom() {
+    return this.#prom
+  }
+
+  get res() {
+    return this.#res
+  }
+
+  get rej() {
+    return this.#rej
+  }
+}
+
+export function encrypt(inDataBuf: Buffer, inKeyBuf: Buffer, inIvBuf: Buffer) {
+  const cipher = crypto.createCipheriv('aes-256-cbc', inKeyBuf, inIvBuf)
+  const buffer = cipher.update(inDataBuf)
+
+  return Buffer.concat([buffer, cipher.final()])
+}
+
+export function decrypt(inDataBuf: Buffer, inKeyBuf: Buffer, inIvBuf: Buffer) {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', inKeyBuf, inIvBuf)
+
+  const buffer = decipher.update(inDataBuf)
+
+  return Buffer.concat([buffer, decipher.final()])
+}
+
+export function md5(inData: Buffer) {
+  const hash = crypto.createHash('md5')
+  hash.update(inData)
+
+  return hash.digest('hex')
+}
+
+export async function tryTimes<R>(
+  inFunc: () => Promise<R>,
+  inOpts?: {
+    times?: number
+    delta?: number
+  }
+) {
+  const times = inOpts?.times || 1
+  const delta = inOpts?.delta || 3000
+  let error: Error = new Error()
+
+  for (let i = 0; i < times; i++) {
+    try {
+      return await inFunc()
+    } catch (inError) {
+      error = inError as Error
+      await new Promise(resolve => setTimeout(resolve, delta))
+    }
+  }
+
+  throw error
 }
