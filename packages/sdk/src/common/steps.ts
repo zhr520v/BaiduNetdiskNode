d159e6311eafa13c1958e653050e5222455bff75
keenghost <keenghost@163.com>
2024-08-29 00:38:09 +0800

    feat: pkg-sdk workflow control steps

---
+++ packages/sdk/src/common/steps.ts
@@ -0,0 +1,111 @@
+export const EStatus = {
+  CREATED: 0,
+  RUNNING: 1,
+  STOPPED: 2,
+  FINISHED: 3,
+}
+
+interface IStepItem {
+  name: string
+  exec: () => Promise<void>
+  stop?: () => Promise<void>
+}
+
+interface ICurrStep {
+  name: string
+  stop: () => Promise<void>
+  res: () => void
+  rej: (inReason?: any) => void
+}
+
+export class Steps {
+  #names: string[] = []
+  #steps: IStepItem[] = []
+  #status: (typeof EStatus)[keyof typeof EStatus] = EStatus.CREATED
+  #error: Error | null = null
+  #step: ICurrStep | null = null
+  #onBeforeRun: () => Promise<void> = async () => {}
+  #onStatusChanged: (inNewStatus: (typeof EStatus)[keyof typeof EStatus]) => void = () => {}
+
+  constructor(inOpts: {
+    steps: IStepItem[]
+    onBeforeRun?: () => Promise<void>
+    onStatusChanged?: (inNewStatus: (typeof EStatus)[keyof typeof EStatus]) => void
+  }) {
+    this.#steps = inOpts.steps
+    this.#onBeforeRun = inOpts.onBeforeRun || this.#onBeforeRun
+    this.#onStatusChanged = inOpts.onStatusChanged || this.#onStatusChanged
+  }
+
+  async run() {
+    if (this.#status === EStatus.FINISHED || this.#status === EStatus.RUNNING) {
+      return
+    }
+
+    this.#error = null
+    this.#setStatus(EStatus.RUNNING)
+
+    await this.#onBeforeRun()
+
+    try {
+      for (const step of this.#steps) {
+        if (this.#names.includes(step.name)) {
+          continue
+        }
+
+        await new Promise<void>((res, rej) => {
+          this.#step = {
+            name: step.name,
+            stop: step.stop || (async () => {}),
+            res: res,
+            rej: rej,
+          }
+
+          step.exec().then(res).catch(rej)
+        })
+
+        this.#names.push(step.name)
+        this.#step = null
+      }
+
+      this.#setStatus(EStatus.FINISHED)
+    } catch (inError) {
+      this.#error = inError as Error
+      this.#setStatus(EStatus.STOPPED)
+    }
+  }
+
+  async stop() {
+    if (this.status !== EStatus.RUNNING) {
+      throw new Error('Stop task failed, task is not RUNNING')
+    }
+
+    if (!this.#step) {
+      return
+    }
+
+    try {
+      await this.#step.stop()
+      this.#step.rej(null)
+    } catch (inError) {
+      this.#step.rej(inError)
+    }
+  }
+
+  #setStatus(inNewStatus: (typeof EStatus)[keyof typeof EStatus]) {
+    this.#status = inNewStatus
+    this.#onStatusChanged(inNewStatus)
+  }
+
+  get name() {
+    return this.#step?.name
+  }
+
+  get status() {
+    return this.#status
+  }
+
+  get error() {
+    return this.#error
+  }
+}
