301a0fa4a56b4a7cd7dd739fa7615663f4a211e7
keenghost <keenghost@163.com>
2024-08-31 01:40:20 +0800

    chore: pkg-sdk add threads tryTimes tryDelta default value

--- packages/sdk/src/common/alpha.ts
+++ packages/sdk/src/common/alpha.ts
@@ -2,3 +2,8 @@ export const __ES__ = !(typeof module !== 'undefined' && typeof module.exports !
 export const __PRESV_ENC_BLOCK_SIZE__ = 32
 
 export type PromType<T> = T extends Promise<infer A> ? A : never
+
+export const __UPLOAD_THREADS__ = 1
+export const __DOWNLOAD_THREADS__ = 1
+export const __TRY_TIMES__ = 3
+export const __TRY_DELTA__ = 3000
