bd3e2f51f9aaf49c306e1a3fe03e1ff7bdbd422e
keenghost <keenghost@163.com>
2025-01-18 12:18:22 +0800

    fix: pkg-srv: appName is required

--- packages/srv/src/requests/proxy-auth.ts
+++ packages/srv/src/requests/proxy-auth.ts
@@ -7,6 +7,11 @@ import { type IContext, type IHttpProxyAuthReq } from '../types/http.js'
 
 export default async (ctx: IContext) => {
   const reqBody = ctx.request.body as IHttpProxyAuthReq
+
+  if (!reqBody.appName) {
+    throw new Error('appName is required')
+  }
+
   const { data } = await axios.post<IAuthRes>(reqBody.addr.replace(/\/*$/, '') + '/api/auth', {
     authCode: reqBody.authCode,
   })
@@ -15,9 +20,9 @@ export default async (ctx: IContext) => {
 
   const newUser: IUserConfig = {
     access_token: data.accessToken,
-    app_id: '',
-    app_key: '',
-    app_name: '',
+    app_id: reqBody.appId,
+    app_key: reqBody.appKey,
+    app_name: reqBody.appName,
     expire: data.expiresIn,
     refresh_token: data.refreshToken,
     secret_key: '',
